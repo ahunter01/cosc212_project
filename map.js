888fa18bb66159d3efdbe9a791f26b96d542fa3f
Angus Hunter <angus.hunter2001@gmail.com>
2021-08-17 15:17:07 +1200

    map now works
    ref #4

--- map.js
+++ map.js
@@ -1,25 +1,27 @@
-
-var map = (function () {
-
-    "use strict";
-
-    var pub = {};
+var Map = (function () {
+    "use strict" ;
     var map;
+    var pub = {};
 
-    pub.setup = function () {
-        map = L.map('map').setView([-45.875, 170.500], 15);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18,attribution: 'Map data &copy; ' +
-            '<a href="http://www.openstreetmap.org/copyright">' + 'OpenStreetMap contributors</a> CC-BY-SA'
-        }).addTo(map);
+    pub.setup = function() {
+
+      map = L.map('map').setView([-45.875, 170.500], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      { maxZoom: 18,
+        attribution: 'Map data &copy; ' +
+        '<a href="http://www.openstreetmap.org/copyright">' +
+        'OpenStreetMap contributors</a> CC-BY-SA'
+            }).addTo(map);
+
     };
-
     return pub;
 }());
 
 if (window.addEventListener) {
-    window.addEventListener('load', Cart_content.setup);
+window.addEventListener('load', Map.setup);
 } else if (window.attachEvent) {
-    window.attachEvent('onload', Cart_content.setup);
+window.attachEvent('onload', Map.setup);
 } else {
-    alert("Could not attach ’Cart_content.setup’ to the ’window.onload’ event");
+alert("Could not attach ’Map.setup’ to the ’window.onload’ event");
 }
